02b3419c1b0b83d70ba476f813aea7ae3f810e04
Gianfranco Martínez Cisneros <francodortmund84@gmail.com>
2024-02-09 09:49:05 -0600

    Se agrega ruta de recepcion de form-urlencoded

--- Parcial1/ServerExpressEnvioDatos/server.js
+++ Parcial1/ServerExpressEnvioDatos/server.js
@@ -4,22 +4,32 @@ const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// Parametros como cadena de consulta
 app.get("/alumnos", (req, res) => {
   console.log(req.query);
   res.send("Respuesta GET del Servidor Express");
 });
 
+// Parametros como parte de la ruta
 app.get("/maestros/:carrera", (req, res) => {
   console.log(req.params.carrera);
   res.send("Respuesta GET del Servidor Express");
 });
 
+// Parametros como JSON en el body
 app.get("/administrativos", (req, res) => {
   for (const campo in req.body) {
-      console.log(req.body[campo]);
+    console.log(req.body[campo]);
   }
 
+  res.send("Respuesta GET del Servidor Express");
+});
+
+// Recepcion de datos de un Formulario
+app.get("/prefectos", (req, res) => {
+  console.log(req.body);
   res.send("Respuesta GET del Servidor Express");
 });
 
