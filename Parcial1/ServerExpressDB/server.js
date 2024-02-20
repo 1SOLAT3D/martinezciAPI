1cea323f6562b2ac97983224d0c3abfdc156fcbc
Gianfranco Martínez Cisneros <francodortmund84@gmail.com>
2024-02-20 09:43:04 -0600

    Se actualiza ejercicio de ServerExpressDB

--- Parcial1/ServerExpressDB/server.js
+++ Parcial1/ServerExpressDB/server.js
@@ -5,26 +5,18 @@ const app = express();
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'worlds2021',
+  database: 'lec2023',
   password: ''
 });
 
 app.get("/equipo", (req, res, next) => {
-  connection.query("SELECT * FROM equipo", (error, results, fields) => {
-    if (error) {
-      console.error("Error al tratar de consultar: ", error);
-      res.status(500).send("Error del servidor");
-      return;
-    }
+  let query = "SELECT * FROM equipo";
 
-    res.send(results);
-  });
-});
+  if (req.query.nombre) {
+    query += " WHERE nombre = ?";
+  }
 
-app.get("/equipo/:acronimo", (req, res, next) => {
-  const acronimo = req.params.acronimo;
-
-  connection.query("SELECT * FROM equipo WHERE acronimo = ?", [acronimo], (error, results, fields) => {
+  connection.query(query, [req.query.nombre], (error, results, fields) => {
     if (error) {
       console.error("Error al tratar de consultar: ", error);
       res.status(500).send("Error del servidor");
